6bf78170e5efba62f4ea60c2a0a14e414b81ad8a
vishalanand23 <Helloworld123>
2015-06-03 22:36:45 -0700

    D

--- java/play/Play.java
+++ java/play/Play.java
@@ -4,22 +4,23 @@ import ai.*;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Play {
     public static void main(String[] args) throws IOException {
         BullsAndCows bullsAndCows = new BullsAndCows();
-        int numberOfGuesses;
-        double totalGuesses = 0.0;
+        double numberOfGuesses;
         int alphabetSize = Integer.valueOf(args[0]);
         int numberOfDigits = Integer.valueOf(args[1]);
         int numberOfTries = Integer.valueOf(args[2]);
+        ArrayList<Double> roundsArray = new ArrayList<Double>();
         for (int i = 0; i < numberOfTries; i++) {
-            AI aI = new CustomAI();
+            AI aI = new CustomAI3Digit();
             String number = new RandomGenerator().generate(numberOfDigits);
             if (number.length() != numberOfDigits) {
                 throw new RuntimeException("Invalid Input");
             }
-            numberOfGuesses = 0;
+            numberOfGuesses = 0.0;
             ArrayList<Round> rounds = new ArrayList<Round>();
             while (true) {
                 String guess = aI.guess(numberOfDigits, rounds);
@@ -30,12 +31,20 @@ public class Play {
                 rounds.add(round);
                 numberOfGuesses++;
                 if (round.getBulls() == numberOfDigits) {
-                    totalGuesses = totalGuesses + numberOfGuesses;
                     break;
                 }
             }
+            roundsArray.add(numberOfGuesses);
+        }
+        Collections.sort(roundsArray);
+        double totalGuesses = 0.0;
+        for (Double round : roundsArray) {
+            totalGuesses+= round;
         }
         double averageGuesses = totalGuesses / numberOfTries;
         System.out.println(averageGuesses);
+        System.out.println(roundsArray.get(0));
+        System.out.println(roundsArray.get(roundsArray.size() - 1));
+        System.out.println(roundsArray.get(Integer.valueOf(roundsArray.size()/2 - 1)));
     }
 }
